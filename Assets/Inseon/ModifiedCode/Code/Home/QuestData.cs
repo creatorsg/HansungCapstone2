c0e7abdb648fb8872d7c95ca3015423bf4b13aff
Won <gmsaturn0917@gmail.com>
2026-05-12 21:31:33 +0900

    erer
    erere

--- Assets/Inseon/ModifiedCode/Code/Home/QuestData.cs
+++ Assets/Inseon/ModifiedCode/Code/Home/QuestData.cs
@@ -9,9 +9,9 @@ public class QuestData : ScriptableObject
     [Header("표시 정보")]
     public string stageName;
 
-    [TextArea(3, 6)] public string questDescription;
-    [TextArea(2, 4)] public string clearCondition;
-    [TextArea(2, 4)] public string recommendCondition;
+    [TextArea(1, 6)] public string questDescription;
+    [TextArea(1, 4)] public string clearCondition;
+    [TextArea(1, 4)] public string recommendCondition;
 
     [Header("등장 적 (최대 4)")]
     public Sprite[] enemyPortraits = new Sprite[4];
