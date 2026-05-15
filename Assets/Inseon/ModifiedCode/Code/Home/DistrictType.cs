b517e841b57b816d9292704abbe265319b7349a8
firefrost612 <firefrost612@gmail.com>
2026-05-15 18:03:25 +0900

    Revert "SDFSDF"
    This reverts commit 69ec8e54eac16192aa1f434940149cfa8787d8bb, reversing
    changes made to 37578195ebfac43f59535c20de32ce566bacf15c.

--- Assets/Inseon/ModifiedCode/Code/Home/DistrictType.cs
+++
@@ -1,8 +0,0 @@
-public enum DistrictType
-{
-    Noble,      // 귀족
-    Cathedral,  // 성당
-    Harbor,     // 부두막
-    Slum,       // 슬럼가
-    Industrial  // 산업단지
-}
--- Assets/Inseon/ModifiedCode/Code/Home/SelectedQuest.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// Home → GamePlay 씬 간 선택된 퀘스트를 넘기기 위한 정적 홀더.
-/// 멀티플레이로 동기화가 필요해지면 SyncVar/Message 기반으로 교체.
-/// </summary>
-public static class SelectedQuest
-{
-    public static QuestData Current;
-}
