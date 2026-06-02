95fd098dae7495cc3579abf94d99506f4fdc911a
JJUN <ljw011226@naver.com>
2026-06-02 11:13:43 +0900

    aa

--- Assets/Jun/Scripts/Battle/HitEffect.cs
+++ Assets/Jun/Scripts/Battle/HitEffect.cs
@@ -1,31 +1,37 @@
 using UnityEngine;
 using System.Collections;
+
 public class HitEffect : MonoBehaviour
 {
-    [SerializeField] private float _duration = 0.5f;
-    [SerializeField] private float firstTime = 2f;
+    [SerializeField] private float _delay = 2f;    // 대기 시간
+    [SerializeField] private float _duration = 0.5f; // 페이드 아웃 시간
+
     void Start() => StartCoroutine(FadeAndDestroy());
 
     private IEnumerator FadeAndDestroy()
     {
         var sr = GetComponent<SpriteRenderer>();
-        if (sr == null) { Destroy(gameObject); yield break; }
+        if (sr == null)
+        {
+            Destroy(gameObject);
+            yield break;
+        }
+        // 대기시간 처리
+        yield return new WaitForSeconds(_delay);
 
+        // 페이드 아웃 처리
         float elapsed = 0f;
-        Color originalColor = sr.color;
+        Color startColor = sr.color;
 
         while (elapsed < _duration)
         {
-            if (elapsed <= firstTime) { elapsed += Time.unscaledDeltaTime; }
-            else
-            {
-                elapsed += Time.unscaledDeltaTime;
-                float alpha = Mathf.Lerp(1f, 0f, elapsed / _duration);
-                sr.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
-                yield return null;
-            }
-        }
+            elapsed += Time.unscaledDeltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsed / _duration);
 
+            sr.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+
+            yield return null;
+        }
         Destroy(gameObject);
     }
 }
